d2579a7ccb5142ee0a3c8fea1ff6a06e95f3e720
annayagyu <asotto4668@gmail.com>
2025-03-17 22:40:51 -0300

    Checkpoint_01

---
+++ src/main/java/br/com/fiap/cheackpoint1/Cheackpoint1Application.java
@@ -0,0 +1,14 @@
+package br.com.fiap.cheackpoint1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cheackpoint1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cheackpoint1Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/fiap/cheackpoint1/Cheackpoint1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cheackpoint1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cheackpoint1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
