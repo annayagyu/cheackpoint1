1827ff3eab0902048714292c76f0908e33ea9ec7
annayagyu <asotto4668@gmail.com>
2025-03-17 22:53:20 -0300

    Fix: problems

--- src/main/java/br/com/fiap/cheackpoint1/Cheackpoint1Application.java
+++ src/main/java/br/com/fiap/cheackpoint1/Cheackpoint1Application.java
@@ -3,6 +3,8 @@ package br.com.fiap.cheackpoint1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class Cheackpoint1Application {
 
